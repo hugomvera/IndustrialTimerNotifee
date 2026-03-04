a6e077de002d170b17628d1707aa9d46a4fcfccc
hugo <HUGOMVERA@GMAIL.COM>
2026-03-04 11:36:26 -0800

    Hello is workign

---
+++ App.js
@@ -0,0 +1,30 @@
+// App.js
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inner}>
+        <Text style={styles.text}>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  inner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
